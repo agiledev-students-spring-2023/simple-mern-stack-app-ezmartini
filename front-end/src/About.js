b120e6477840e29f6f23ea18161f9b41487bc182
ezmartini <em4047@nyu.edu>
2023-02-13 17:37:29 -0500

    updated styles

--- front-end/src/About.js
+++ front-end/src/About.js
@@ -6,14 +6,24 @@ export default function About() {
     <>
       {' '}
       <h1> About Us </h1>
-      <img src={Photo} style={{ width: '80%' }} />
-      <p>
-        {' '}
-        My name is Eduarda and I am currently a senior at NYU studying CS. I was
-        born in Brazil but was raised on Cape Cod, Massachusetts. In my
-        freetime, I like to listen to music, play around in Illustrator, and
-        hangout with my roommate and my friends.{' '}
-      </p>
+      <div style={{ padding: '20px' }}>
+        <img
+          src={Photo}
+          style={{
+            width: '50%',
+            maxWidth: '400px',
+            display: 'inline-block',
+            verticalAlign: 'top',
+          }}
+        />
+        <p style={{ width: '50%', display: 'inline-block' }}>
+          {' '}
+          My name is Eduarda and I am currently a senior at NYU studying CS. I
+          was born in Brazil but was raised on Cape Cod, Massachusetts. In my
+          freetime, I like to listen to music, play around in Illustrator, and
+          hangout with my roommate and my friends.{' '}
+        </p>
+      </div>
     </>
   )
 }
