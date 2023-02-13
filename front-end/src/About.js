989fed514494b57f19217c0be115fbad784868f2
ezmartini <em4047@nyu.edu>
2023-02-13 12:56:48 -0500

    added textual content to about us page

--- front-end/src/About.js
+++ front-end/src/About.js
@@ -5,7 +5,13 @@ export default function About() {
     <>
       {' '}
       <h1> About Us </h1>
-      <p> </p>
+      <p>
+        {' '}
+        My name is Eduarda and I am currently a senior at NYU studying CS. I was
+        born in Brazil but was raised on Cape Cod, Massachusetts. In my
+        freetime, I like to listen to music, play around in Illustrator, and
+        hangout with my roommate and my friends.{' '}
+      </p>
     </>
   )
 }
